8f5a47d68131608273a6e6db10d817646d0cbc2c
Yuichiro Tsuji <t.yuyu.alies@gmail.com>
2022-05-02 22:15:17 +0900

    add rust hello world project

---
+++ hello_world_rust/target/debug/deps/hello_world_rust-0735e05253d03e22.d
@@ -0,0 +1,5 @@
+/Users/yuichiro/workspace/tutorials/hello_world_rust/target/debug/deps/hello_world_rust-0735e05253d03e22: src/main.rs
+
+/Users/yuichiro/workspace/tutorials/hello_world_rust/target/debug/deps/hello_world_rust-0735e05253d03e22.d: src/main.rs
+
+src/main.rs:
---
+++ hello_world_rust/target/debug/hello_world_rust.d
@@ -0,0 +1,1 @@
+/Users/yuichiro/workspace/tutorials/hello_world_rust/target/debug/hello_world_rust: /Users/yuichiro/workspace/tutorials/hello_world_rust/src/main.rs
